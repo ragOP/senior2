aee4afad426964ce4c29b4047a9c19b2896d2a06
farish-jamal <farishjamal98@gmail.com>
2024-03-29 03:32:27 +0530

    fix

---
+++ src/pages/Two.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Two = () => {
+  return (
+    <div>
+     <iframe
+        title="External Content"
+        src="https://b2-qmk2-e05tottbv-ragop1.vercel.app/"
+        style={{ width: '100%', height: '100vh', border: 'none' }}
+      />
+    </div>
+  )
+}
+
+export default Two
